2f1e27b140b7ae1d28c2eccac91359c4424d0cb0
Mario Rodriguez <20193tn165@utez.edu.mx>
2024-06-28 23:11:09 -0600

    Update
    Uso de componente react-bootstrap y react-router-dom.

---
+++ src/layouts/navBar.js
@@ -0,0 +1,35 @@
+import { Fragment } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link, Outlet } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Contact</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+              <Link>Client</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <section>
+        <Outlet>
+
+        </Outlet>
+      </section>
+    </>
+  );
+};
+
+export default NavBar;
