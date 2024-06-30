0b93b1e0f6ec39bf62929456e933b7b4e7e85321
Mario Rodriguez <20193tn165@utez.edu.mx>
2024-06-29 20:08:17 -0600

    Commit NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import './App.css'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import About from './components/about';
+import Contact from './components/contact';
+import Home from './components/home';
+import NavBarMain from './layouts/navBar'
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World!</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <NavBarMain/> }>
+          <Route index element = { <Home/> }/>
+          <Route path="about" element = { <About/> }/>
+          <Route path="contact" element = { <Contact/> }/>
+
+          <Route path="*" element={ <Navigate replace to = "/"/>} />
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/layouts/navBar.js
+++ src/layouts/navBar.js
@@ -1,32 +1,54 @@
-import { Fragment } from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link, Outlet } from "react-router-dom";
+import "../App.css";
+import { FaSearch, FaUser } from "react-icons/fa";
+import {
+  Form,
+  Navbar,
+  Container,
+  Nav,
+  InputGroup,
+  FormControl,
+  Image,
+} from "react-bootstrap";
+import Logo from "../images/logo.png";
 
 const NavBar = () => {
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar expand="lg" variant="dark" className="navBg">
+        <Container fluid>
+          <Navbar.Brand className="ms-2" as={Link} to="/">
+            <Image src={Logo} rounded />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Contact</Nav.Link>
-              <Nav.Link href="#link">About</Nav.Link>
-              <Link>Client</Link>
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-between"
+          >
+            <Form className="d-flex flex-grow-1 mx-lg-3 my-2 my-lg-0">
+              <InputGroup>
+                <FormControl
+                  placeholder="Buscar"
+                  aria-label="Buscar"
+                  aria-describedby="basic-addon1"
+                />
+                <InputGroup.Text id="basic-addon1">
+                  <FaSearch />
+                </InputGroup.Text>
+              </InputGroup>
+            </Form>
+            <Nav className="ms-auto" as={Link} to="about">
+              <Nav.Link href="#profile">
+                <FaUser style={{ marginRight: "5px" }} />
+                Perfil
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
       <section>
-        <Outlet>
-
-        </Outlet>
+        <Outlet></Outlet>
       </section>
     </>
   );
